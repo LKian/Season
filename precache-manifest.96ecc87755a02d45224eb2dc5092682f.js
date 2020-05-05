5b34c12dc1f595a95bbe848134ecf0007e92e4a0
Leah <LeahKahwadjian@gmail.com>
2020-05-04 23:32:12 -0400

    Updates

---
+++ precache-manifest.96ecc87755a02d45224eb2dc5092682f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "406b0af7d010ccef554ffa794910d4fe",
+    "url": "/season/index.html"
+  },
+  {
+    "revision": "ba25b2d26950208ee3ae",
+    "url": "/season/static/js/2.70b4bc4e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/season/static/js/2.70b4bc4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d06f7751ced7e220d9c6",
+    "url": "/season/static/js/main.72842553.chunk.js"
+  },
+  {
+    "revision": "8c43926407c37f51244c",
+    "url": "/season/static/js/runtime-main.2e3c0b4e.js"
+  }
+]);
